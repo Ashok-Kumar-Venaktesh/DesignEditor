dcb0fc24516367be235fb9df9ae5c5329541fe19
Ashok Kumar V <28ash92@gmail.com>
2018-02-21 14:40:23 +0530

    base url changed in the app.js

--- app/app.js
+++ app/app.js
@@ -55,7 +55,7 @@ directive('ngCanvas', function(db_operation){
         template: '<canvas id="canvas" width="1200px" height="600px"></canvas>',
         controller: function ($scope, $timeout, db_operation) {
 
-          $scope.baseURI = 'http://localhost:5000';
+          $scope.baseURI = 'https://sleepy-hamlet-79903.herokuapp.com/';
           $scope.history = [];
 
           $scope.get_options = function(){
